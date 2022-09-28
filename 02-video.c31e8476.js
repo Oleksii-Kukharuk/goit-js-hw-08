9dee3a39445b62cc9dbca468228507445061c25f
Oleksii-Kukharuk <104523043+Oleksii-Kukharuk@users.noreply.github.com>
2022-09-28 20:41:26 +0000

    Deploying to gh-pages from @ Oleksii-Kukharuk/goit-js-hw-08@742ce703a63b768c6661f99623599b0a405b1d37 🚀

---
+++ 02-video.c31e8476.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("iframe"),o=new Vimeo.Player(e);o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)})),o.setLoop(!0).then((function(e){})).catch((function(e){}))}();
+//# sourceMappingURL=02-video.c31e8476.js.map
