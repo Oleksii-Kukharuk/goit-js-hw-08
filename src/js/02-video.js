742ce703a63b768c6661f99623599b0a405b1d37
Oleksii-Kukharuk <104523043+Oleksii-Kukharuk@users.noreply.github.com>
2022-09-28 23:40:59 +0300

    second task in progress

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player
+  .setLoop(true)
+  .then(function (loop) {
+    // loop was turned on
+  })
+  .catch(function (error) {
+    // an error occurred
+  });
