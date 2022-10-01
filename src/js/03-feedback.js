5630f066d793e6eb669a96e668d45ff63d4e9269
Oleksii-Kukharuk <104523043+Oleksii-Kukharuk@users.noreply.github.com>
2022-10-01 20:01:13 +0300

    3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
+import { save } from "./storage";
+import { load } from "./storage";
+import { remove } from "./storage";
 
+const inputRef = document.querySelector('.feedback-form');
+const LOCAL_DATA_STORAGE = 'feedback-form-state';
+
+
+
+initPage();
+
+const onInputSave = event => {
+  const { name, value } = event.target;
+
+    let saveDate = load(LOCAL_DATA_STORAGE);
+    saveDate = saveDate ? saveDate : {};
+    saveDate[name] = value;
+
+    save(LOCAL_DATA_STORAGE, saveDate);
+
+};
+
+const throttledOnInputSave = throttle(onInputSave, 1000);
+inputRef.addEventListener('input', throttledOnInputSave);
+
+function initPage() {
+  const saveDate = load(LOCAL_DATA_STORAGE);
+  if (!saveDate) {
+    return;
+  }
+  Object.entries(saveDate).forEach(([name, value]) => {
+    inputRef.elements[name].value = value;
+  });
+
+
+const handleSubmit = event => {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  console.log({ email: email.value, message: message.value });
+  event.currentTarget.reset();
+
+  remove(LOCAL_DATA_STORAGE);
+};
+inputRef.addEventListener('submit', handleSubmit);
